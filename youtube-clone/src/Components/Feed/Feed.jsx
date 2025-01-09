cf9d31110d75614428a82831afd35a80d9faf8d6
Shubham Giri <shubhamgiri7631.sg@gmail.com>
2025-01-09 17:27:22 +0530

    fetched data using api for youtube

--- youtube-clone/src/Components/Feed/Feed.jsx
+++ youtube-clone/src/Components/Feed/Feed.jsx
@@ -1,145 +1,38 @@
 import "./Feed.css";
-import thumbnail1 from "../../assets/thumbnail1.png";
-import thumbnail2 from "../../assets/thumbnail2.png";
-import thumbnail3 from "../../assets/thumbnail3.png";
-import thumbnail4 from "../../assets/thumbnail4.png";
-import thumbnail5 from "../../assets/thumbnail5.png";
-import thumbnail6 from "../../assets/thumbnail6.png";
-import thumbnail7 from "../../assets/thumbnail7.png";
-import thumbnail8 from "../../assets/thumbnail8.png";
-import { Link } from "react-router-dom";
 
-const Feed = () => {
+import { Link } from "react-router-dom";
+import { API_KEY, value_converter } from "../../data";
+import { useEffect, useState } from "react";
+
+
+const Feed = ({ category }) => {
+  const [data, setData] = useState([]);
+  const fetchData = async () => {
+    const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`;
+
+    await fetch(videoList_url)
+      .then((res) => res.json())
+      .then((data) => setData(data.items));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [category]);
+
   return (
     <div className="feed">
-      <Link to={`video/20/4521`} className="card">
-        <img src={thumbnail1} alt="" />
-        <h2>
-          Best channel to learn coading that help you to be a great developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </Link>
-      <div className="card">
-        <img src={thumbnail2} alt="" />
-        <h2>
-          Best channel to learn coading that help you to be a great developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail3} alt="" />
-        <h2>
-          Best channel to learn coading that help you to be a great developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail4} alt="" />
-        <h2>
-          Best channel to learn coading that help you to be a great developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail5} alt="" />
-        <h2>
-          Best channel to learn coading that help you to be a great developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail6} alt="" />
-        <h2>
-          Best channel to learn coading that help you to be a great developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail7} alt="" />
-        <h2>
-          Best channel to learn coading that help you to be a great developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail8} alt="" />
-        <h2>
-          Best channel to learn coading that help you to be a great developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail1} alt="" />
-        <h2>
-          Best channel to learn coading that help you to be a great developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail2} alt="" />
-        <h2>
-          Best channel to learn coading that help you to be a great developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail3} alt="" />
-        <h2>
-          Best channel to learn coading that help you to be a great developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail4} alt="" />
-        <h2>
-          Best channel to learn coading that help you to be a great developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail5} alt="" />
-        <h2>
-          Best channel to learn coading that help you to be a great developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail6} alt="" />
-        <h2>
-          Best channel to learn coading that help you to be a great developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail7} alt="" />
-        <h2>
-          Best channel to learn coading that help you to be a great developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail8} alt="" />
-        <h2>
-          Best channel to learn coading that help you to be a great developer
-        </h2>
-        <h3>Greatstack</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
+      {data.map((item) => (
+        <Link
+          to={`video/${item.snippet.categoryId}/${item.id}`}
+          className="card"
+          key={item.id}
+        >
+          <img src={item.snippet.thumbnails.medium.url} alt="" />
+          <h2>{item.snippet.title}</h2>
+          <h3>{item.snippet.channelTitle}</h3>
+          <p>{value_converter(item.s)} &bull; 2 days ago</p>
+        </Link>
+      ))}
     </div>
   );
 };
--- youtube-clone/src/Components/PlayVideo/PlayVideo.jsx
+++ youtube-clone/src/Components/PlayVideo/PlayVideo.jsx
@@ -1,5 +1,177 @@
+import "./PlayVideo.css";
+import video1 from "../../assets/video.mp4";
+import like from "../../assets/like.png";
+import dislike from "../../assets/dislike.png";
+import share from "../../assets/share.png";
+import save from "../../assets/save.png";
+import jack from "../../assets/jack.png";
+import user_profile from "../../assets/user_profile.jpg";
+
 const PlayVideo = () => {
-  return <div>PlayVideo</div>;
+  return (
+    <div className="play-video">
+      <video src={video1} controls autoPlay muted></video>
+      <h3>Best youtube channel to learn webdevelopment</h3>
+      <div className="play-video-info">
+        <p>12,345 views &bull; 2 days ago</p>
+        <div>
+          <span>
+            <img src={like} alt="" /> 183
+          </span>
+          <span>
+            <img src={dislike} alt="" />
+          </span>
+          <span>
+            <img src={share} alt="" /> Share
+          </span>
+          <span>
+            <img src={save} alt="" /> Save
+          </span>
+        </div>
+      </div>
+      <hr />
+      <div className="publisher">
+        <img src={jack} alt="" />
+        <div>
+          <p>GreatStack</p>
+          <span>1M Subscribers</span>
+        </div>
+        <button>Subscribe</button>
+      </div>
+      <div className="vid-description">
+        <p>Channel that makes learning easy</p>
+        <p>Subscribe greatstack to watch more tutorials on web development</p>
+        <hr />
+        <h4>130 comments</h4>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              A global computer Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Harum quam quisquam nemo maxime totam fuga ipsum
+              eos minima quaerat repellendus.
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>231</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              A global computer Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Harum quam quisquam nemo maxime totam fuga ipsum
+              eos minima quaerat repellendus.
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>231</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              A global computer Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Harum quam quisquam nemo maxime totam fuga ipsum
+              eos minima quaerat repellendus.
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>231</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              A global computer Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Harum quam quisquam nemo maxime totam fuga ipsum
+              eos minima quaerat repellendus.
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>231</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              A global computer Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Harum quam quisquam nemo maxime totam fuga ipsum
+              eos minima quaerat repellendus.
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>231</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              A global computer Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Harum quam quisquam nemo maxime totam fuga ipsum
+              eos minima quaerat repellendus.
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>231</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="comment">
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>
+              A global computer Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Harum quam quisquam nemo maxime totam fuga ipsum
+              eos minima quaerat repellendus.
+            </p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>231</span>
+              <img src={dislike} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PlayVideo;
